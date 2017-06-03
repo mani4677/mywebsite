6f00b62d8ebec8b3eb6d6d5cbeb5325ef820fb2f
Muhammad Usman <manidistrusted@gmail.com>
2017-06-03 14:59:14 +0500

    yes i have added the index file

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+?>
+
+
+<html>
+<head>
+	<title>Git Hub Project</title>
+</head>
+<body>
+
+<h1>Git Hub Practice</h1>
+
+
+<p>This web page is only refering to the practice that i have done for using the git</p>
+
+</body>
+</html>
